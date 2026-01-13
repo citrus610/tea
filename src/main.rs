acd7123895a72a574616378ac3951d4cba63af94
citrus610 <79373642+citrus610@users.noreply.github.com>
2026-01-13 18:49:01 +0700

    clean up main

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,20 @@
 #![allow(dead_code)]
+use std::time::Instant;
 
-use crate::chess::{bitboard::Bitboard, board::Board, movegen::perft, moves::{Move, MoveKind}, square::Square};
+use crate::chess::{board::Board, movegen::perft};
 
 mod chess;
 
 fn main() {
     println!("Hello, world!");
 
-    // for a in 0..64 {
-    //     for b in 0..64 {
-    //         let m = Bitboard::from_line(Square::from_raw(a), Square::from_raw(b)) | Bitboard::from_raw(1 << a) | Bitboard::from_raw(1 << b);
-
-    //         println!("{}", m);
-
-    //         let mut input = String::new();
-
-    //         std::io::stdin().read_line(&mut input).expect("w");
-    //     }
-    // }
-
     let mut board = Board::startpos().unwrap();
 
-    // board.make(Move::new(Square::A2, Square::A4, MoveKind::Normal));
-    // board.make(Move::new(Square::A7, Square::A6, MoveKind::Normal));
-    // board.make(Move::new(Square::A4, Square::A5, MoveKind::Normal));
-    // board.make(Move::new(Square::B7, Square::B5, MoveKind::Normal));
+    let time = Instant::now();
+    let count = perft::<true>(&mut board, 7);
+    let elasped = time.elapsed().as_millis();
+    let knps = count as u128 / elasped;
 
-    println!("{}", perft::<true>(&mut board, 7));
+    println!("perft: {}", count);
+    println!("nps: {} kn/s", knps);
 }
